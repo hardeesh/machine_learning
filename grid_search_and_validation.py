f2faff441ee80a36b573bd2731c9b106e5a6695e
Hardeesh <hardeesh.s@gmail.com>
2024-05-03 16:07:11 +0100

    finally works

--- grid_search_and_validation.py
+++ grid_search_and_validation.py
@@ -1,16 +1,17 @@
-from sklearn import datasets
+from sklearn import datasets,  ensemble
 import numpy as np
 from sklearn.ensemble import GradientBoostingRegressor
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split, RandomizedSearchCV
-from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import mean_squared_error, confusion_matrix, ConfusionMatrixDisplay
 from scipy.stats import uniform, randint
+
 
 X, y = datasets.load_diabetes(return_X_y=True)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 
-distributions = {'n_estimators' : uniform(100, 1000),
+distributions = {'n_estimators' : randint(100, 1000),
                 'learning_rate' : uniform(0.01, 0.2),
                 'subsample' : uniform(0.5, 0.5),
                 'max_depth' : randint(4, 10)}
@@ -23,16 +24,11 @@ randomised_search.fit(X_train, y_train)
 #using the best parameters
 best_gb_params = randomised_search.best_params_
 
-best_model = GradientBoostingRegressor(**best_gb_params).fit(X_train, y_train)
+best_model = ensemble.GradientBoostingRegressor(**best_gb_params).fit(X_train, y_train)
 
 y_pred = best_model.predict(X_test)
 
-accuracy = accuracy_score(y_test, y_pred, normalize=True)
-print("Validation Accuracy:", accuracy)
+mse = mean_squared_error(y_test, y_pred)
+print("Mean Squared Error:", mse)
 print("Best Hyperparameters:", best_gb_params)
 
-cm = confusion_matrix(y_test, y_pred)
-disp = ConfusionMatrixDisplay(confusion_matrix=cm)
-
-disp.plot()
-plt.show()
