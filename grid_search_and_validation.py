a6edd1ca4075e85f1c39dcd38a2da6ef96f12ad4
Hardeesh <hardeesh.s@gmail.com>
2024-05-03 15:56:43 +0100

    uses regressor instead of classifier

--- grid_search_and_validation.py
+++ grid_search_and_validation.py
@@ -1,41 +1,31 @@
 from sklearn import datasets
 import numpy as np
-from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import GradientBoostingRegressor
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split, RandomizedSearchCV
 from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
 from scipy.stats import uniform, randint
-import random
 
 X, y = datasets.load_diabetes(return_X_y=True)
-print(X.shape)
-print(y.shape)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 
-random_float = random.uniform(0, 1) 
-random_integer_est = random.randint(100, 1000) 
-random_integer = random.randint(4, 10)
+distributions = {'n_estimators' : uniform(100, 1000),
+                'learning_rate' : uniform(0.01, 0.2),
+                'subsample' : uniform(0.5, 0.5),
+                'max_depth' : randint(4, 10)}
 
-gb_classifier = GradientBoostingClassifier(n_estimators=random_integer_est, 
-                                           learning_rate=random_float, 
-                                           subsample=random_float,
-                                           max_depth=random_integer).fit(X_train, y_train)
+gb_regressor = GradientBoostingRegressor()
 
-distributions = dict(n_estimators=random_integer_est, 
-                                           learning_rate=random_float, 
-                                           subsample=random_float,
-                                           max_depth=random_integer)
-
-randomised_search = RandomizedSearchCV(gb_classifier, distributions, random_state=0)
+randomised_search = RandomizedSearchCV(estimator = gb_regressor, param_distributions=distributions)
 randomised_search.fit(X_train, y_train)
 
 #using the best parameters
 best_gb_params = randomised_search.best_params_
 
-best_score = GradientBoostingClassifier(**best_gb_params, random_state=42).fit(X_train, y_train)
+best_model = GradientBoostingRegressor(**best_gb_params).fit(X_train, y_train)
 
-y_pred = best_score.predict(X_test)
+y_pred = best_model.predict(X_test)
 
 accuracy = accuracy_score(y_test, y_pred, normalize=True)
 print("Validation Accuracy:", accuracy)
